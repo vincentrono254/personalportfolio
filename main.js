d68eee54b4eb73abb32de82fa7db2dfdff664369
vincentrono254 <vincentmatson23@gmail.com>
2024-05-08 22:43:26 +0300

    responsive nabar modified

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
-var sidemenu = document.getElementById("sidemenu");
+// var sidemenu = document.getElementById("sidemenu");
 
-function openmenu() {
-  sidemenu.style.right = "0";
-}
-function closemenu() {
-  sidemenu.style.right = "-200px";
-}
-var tablinks = document.getElementsByClassName("tab-links");
-var tabcontents = document.getElementsByClassName("tab-contents");
+// function openmenu() {
+//   sidemenu.style.right = "0";
+// }
+// function closemenu() {
+//   sidemenu.style.right = "-200px";
+// }
+// var tablinks = document.getElementsByClassName("tab-links");
+// var tabcontents = document.getElementsByClassName("tab-contents");
 
-function opentab(tabname) {
-  for (tablink of tablinks) {
-    tablink.classList.remove("active-link");
-  }
-  for (tabcontent of tabcontents) {
-    tabcontent.classList.remove("active-tab");
-  }
-  document.getElementById(tabname).classList.add("active-tab");
-  event.currentTarget.classList.add("active-link");
-}
+// function opentab(tabname) {
+//   for (tablink of tablinks) {
+//     tablink.classList.remove("active-link");
+//   }
+//   for (tabcontent of tabcontents) {
+//     tabcontent.classList.remove("active-tab");
+//   }
+//   document.getElementById(tabname).classList.add("active-tab");
+//   event.currentTarget.classList.add("active-link");
+// }
